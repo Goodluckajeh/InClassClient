58acdc07f33751b55adc25e0762c1acfe7f2f09f
Goodluck <143440350+Goodluckajeh@users.noreply.github.com>
2025-09-14 12:54:32 -0500

    created API call

---
+++ Controllers/APICall.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class APICallController : ControllerBase
+{
+    private static readonly HttpClient _httpClient = new HttpClient();
+
+    // Asynchronous method to call the Joke API and return the raw JSON response as a string
+    [HttpGet("joke")]
+    public async Task<IActionResult> GetJokeApiDataAsync()
+    {
+        var url = "https://v2.jokeapi.dev/joke/Any";
+        var response = await _httpClient.GetAsync(url);
+
+        response.EnsureSuccessStatusCode();
+
+        string data = await response.Content.ReadAsStringAsync();
+        return Content(data, "application/json");
+    }
+}
